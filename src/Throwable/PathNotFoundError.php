3435f7b56098f5d5d64c3d5a85bf26c5b750fee5
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-10 10:20:38 +0200

    Update PathNotFoundError.php

--- src/Throwable/PathNotFoundError.php
+++ src/Throwable/PathNotFoundError.php
@@ -6,7 +6,7 @@
  * Licensed under the MIT License (https://opensource.org/licenses/MIT)
  */
 
-namespace Delight\I18n\Throwable;
+namespace Pinga\Locale\Throwable;
 
 /** Error that is thrown when the specified path could not be found */
 class PathNotFoundError extends Error {}
